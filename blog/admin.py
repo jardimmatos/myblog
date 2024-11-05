f0ad3c972d5e7c60f21d79714d68f0722c732871
Agostinho <jr.jardimmatos@gmail.com>
2024-11-05 09:36:11 -0300

    Admin para a Model de Post

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 from . import models
 
+
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'published', 'status')
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    save_as = True
+    date_hierarchy = 'published'
+
+    def save_model(self, request, obj, form, change):
+        """
+        Registrar usuário que criou o post via admin
+        """
+        if not change:
+            obj.created_by = request.user
+        super().save_model(request, obj, form, change)
