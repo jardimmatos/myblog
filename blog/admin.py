de3dccc3bf2c1a90caae847652bc8228850d1841
Agostinho <jr.jardimmatos@gmail.com>
2024-11-05 15:35:39 -0300

    fixed: ajuste de nome de campos para date_hierarchy e list_display

--- blog/admin.py
+++ blog/admin.py
@@ -4,11 +4,13 @@ from . import models
 
 @admin.register(models.Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'published', 'status')
+    list_display = ('title', 'slug', 'created_at', 'status')
     list_filter = ('status',)
+    search_fields = ('title',)
     prepopulated_fields = {'slug': ('title',)}
     save_as = True
-    date_hierarchy = 'published'
+    date_hierarchy = 'created_at'
+    raw_id_fields = ('created_by',)
 
     def save_model(self, request, obj, form, change):
         """
