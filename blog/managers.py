d325b5970f58d378c22e4fdc58df84462c375cdb
Agostinho <jr.jardimmatos@gmail.com>
2024-11-05 15:33:14 -0300

    chore: Adequando um manager exclusivo para ListView apenas de posts publicados

---
+++ blog/managers.py
@@ -0,0 +1,8 @@
+from . import enums
+from django.db import models
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return (
+            super().get_queryset().filter(status=enums.StatusEnum.published.name)
+		)
